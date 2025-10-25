af04634e4c0b89edb841e6b49fa025a161dafdfb
Victor Ganson <vicganson@gmail.com>
2025-10-25 13:15:51 -0400

    Implement SearchBar class with search functionality

---
+++ search.java
@@ -0,0 +1,25 @@
+import java.util.HashMap
+import java.util.Map
+public class SearchBar{
+  HashMap<> listedItems;
+  String search;
+  
+  public SearchBar(l, s){
+    this.listedItems = l;
+    this.search = s;
+  }
+
+  public HashMap<> searchResults(String s){
+    
+      
+  }
+  
+
+
+
+
+
+
+  
+
+}
