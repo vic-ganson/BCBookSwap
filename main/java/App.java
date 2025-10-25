044d7c434b0e55e28116fed081056aa780a933bc
Victor Ganson <vicganson@gmail.com>
2025-10-25 16:59:32 -0400

    Update App.java

--- main/java/App.java
+++ main/java/App.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
