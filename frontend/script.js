3ac7a3567757a495f5110da7d842a2c64a935cbf
HenrySchilling <hankbobmc@gmail.com>
2025-10-25 13:33:45 -0400

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,13 @@
+function toggleVisible() {
+  const passwordInput = document.getElementById("loginPassword");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+  passwordInput.type = "password";
+  }
+}
 
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // prevent real form submission
+  alert("Login attempted for user: " + document.getElementById("loginUsername").value);
+});
