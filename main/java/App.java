cb5d81346fa07e2a6a6c34d38505597540307514
Victor Ganson <vicganson@gmail.com>
2025-10-25 17:13:29 -0400

    Update App.java

--- main/java/App.java
+++ main/java/App.java
@@ -7,34 +7,6 @@ import java.util.Map;
 import java.util.List;
 
 public class App {
-
-  private HashMap<Account, List<Textbook> > listings;
-
-  public App(){
-    this.listings = new HashMap<>();
-  }
-  
-  public void addListing(Account seller, String code, String title, String author, String ISBN, String price, String condition){
-    Textbook book = new Textbook(seller, code, title, author, ISBN, price, condition);
-    if (listings.contains(seller)){
-      priorBooks = listings.get(seller);
-      currentBooks = priorBooks.add(book);
-      listings.put(seller, currentBooks);
-    }
-    else{
-      List<Textbook> books = book;
-      listings.put(seller, books);
-    }
-  }
-  public void removeListing(Account seller, Textbook book){
-    priorBooks = listings.get(seller);
-    currentBooks = priorBooks.remove(book);
-    listings.put(seller, currentBooks);
-  }
-  public void removeSeller(Account seller){
-    listings.remove(seller);
-  }
-
   
   public static void main(String[] args){
     SpringApplication.run(App.class, args);
