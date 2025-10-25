ce604497c68b122ae374b822c83864b12801e951
HenrySchilling <hankbobmc@gmail.com>
2025-10-25 15:13:54 -0400

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -3,11 +3,52 @@ function toggleVisible() {
   if (passwordInput.type === "password") {
     passwordInput.type = "text";
   } else {
-  passwordInput.type = "password";
+    passwordInput.type = "password";
   }
 }
+function toggleVisible2() {
+  const passwordInput = document.getElementById("loginPassword2");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+}
+
+//password checking
+function checkCredentials(){
+  let usernameEntry = document.getElementById('loginUsername').value;
+  let passwordEntry = document.getElementById('loginPassword').value;
+
+}
+
+//ON LOGIN FORM SUBMIT
+try{
+  document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    checkCredentials()
+  });
+} catch(e){}
+
+
+
+
+
+// create account button from login
+document.querySelector(".createAccountButton").addEventListener("click", function() {
+  // send to createaccount page
+  window.location.href = "createAccount.html";
+});
+
+document.querySelector(".loginButton").addEventListener("click", function() {
+  // send to login page
+  alert("This is a popup message!");
+  window.location.href = "index.html";
+});
+
 
 document.getElementById("loginForm").addEventListener("submit", function(event) {
   event.preventDefault(); // prevent real form submission
   alert("Login attempted for user: " + document.getElementById("loginUsername").value);
 });
+const API_URL = "https://hacktheheightsproject.onrender.com";
