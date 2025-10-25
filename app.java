421ebb99a3964451ea9baa7ef498287b7c72ed9f
Victor Ganson <vicganson@gmail.com>
2025-10-25 16:27:39 -0400

    Update app.java

--- app.java
+++ app.java
@@ -1,3 +1,7 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
@@ -30,11 +34,10 @@ public class App {
   public void removeSeller(Account seller){
     listings.remove(seller);
   }
-  public static void main(String[] args){
-  }
-
-
 
   
-
+  public static void main(String[] args){
+    SpringApplication.run(App.class, args);
+  }
+  
 }
