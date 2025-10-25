de6e25018da84506104c863403a685f86fe141f9
Victor Ganson <vicganson@gmail.com>
2025-10-25 15:16:12 -0400

    Update app.java

--- app.java
+++ app.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 
 public class App {
 
-  private HashMap<String, List<Textbook> > listings;
+  private HashMap<Account, List<Textbook> > listings;
 
   public App(){
     this.listings = new HashMap<>();
   }
   
-  public void addListing(String seller, String code, String title, String author, String ISBN, String price, String condition){
+  public void addListing(Account seller, String code, String title, String author, String ISBN, String price, String condition){
     Textbook book = new Textbook(seller, code, title, author, ISBN, price, condition);
     if (listings.contains(seller)){
       priorBooks = listings.get(seller);
@@ -22,12 +22,12 @@ public class App {
       listings.put(seller, books);
     }
   }
-  public void removeListing(String seller, Textbook book){
+  public void removeListing(Account seller, Textbook book){
     priorBooks = listings.get(seller);
     currentBooks = priorBooks.remove(book);
     listings.put(seller, currentBooks);
   }
-  public void removeSeller(String seller){
+  public void removeSeller(Account seller){
     listings.remove(seller);
   }
   public static void main(String[] args){
