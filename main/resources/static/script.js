1dc9d5e179b74606f63e702a63289ba3a2fc0709
HenrySchilling <hankbobmc@gmail.com>
2025-10-25 17:09:21 -0400

    Update script.js

--- main/resources/static/script.js
+++ main/resources/static/script.js
@@ -16,10 +16,9 @@ function toggleVisible2() {
 }
 
 //password checking
-function checkCredentials(){
+function validEmail(){
   let usernameEntry = document.getElementById('loginUsername').value;
-  let passwordEntry = document.getElementById('loginPassword').value;
-
+  return usernameEntry.endsWith("@bc.edu");
 }
 
 
@@ -27,10 +26,35 @@ function checkCredentials(){
 try{
   document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    checkCredentials()
+    if(!validEmail()){
+      alert("You must login with a Boston College email address (@bc.edu)");
+    } else {
+      //check usernames etc
+      let usernameEntry = document.getElementById('loginUsername').value;
+      let passwordEntry = document.getElementById('loginPassword').value;
+    }
   });
 } catch(e){}
 
+
+//ON CREATE ACCOUNT FORM SUBMIT
+try{
+  document.getElementById("createAccountForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let usernameEntry = document.getElementById('loginUsername').value;
+    let passwordEntry = document.getElementById('loginPassword').value;
+    let passwordEntry2 = document.getElementById('loginPassword2').value;
+    if(!validEmail()){
+      alert("You must login with a Boston College email address (@bc.edu)");
+    } else {
+      if(passwordEntry != passwordEntry2){
+      alert("Your password entries must match.")
+    } else {
+      //save account
+    }
+    }
+  });
+} catch(e){}
 
 
 // create account button from login
