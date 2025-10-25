c03c6f27ec68db7bf26a2f20cf17b6cc74514407
Victor Ganson <vicganson@gmail.com>
2025-10-25 13:45:25 -0400

    Refactor SearchBar class and improve search functionality

--- search.java
+++ search.java
@@ -1,25 +1,22 @@
 import java.util.HashMap
 import java.util.Map
 public class SearchBar{
-  HashMap<> listedItems;
-  String search;
+  private HashMap<String, Textbook> listedItems;
+  private String search;
   
-  public SearchBar(l, s){
+  public SearchBar(HashMap<String,Textbook> l, String s){
     this.listedItems = l;
     this.search = s;
   }
 
-  public HashMap<> searchResults(String s){
-    
-      
+  public HashMap<> searchResults(String query){
+    Map<String, Textbook> results = new HashMap<>();
+    for (String s : listedItems){
+      Textbook t = listedItems.get(s);
+      if (t.getCourseCode().contains(query) || t.getTitle().contains(query) || t.getAuthor().contains(query) || t.getISBN().contains(query)){
+        results.put(s, t);
+      }
+    }
+    return results;
   }
-  
-
-
-
-
-
-
-  
-
 }
