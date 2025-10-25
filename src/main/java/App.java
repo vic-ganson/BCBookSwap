30006788a2f30400ab407fc825722506ea1ad5ca
Victor Ganson <vicganson@gmail.com>
2025-10-25 18:03:09 -0400

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,8 +2,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.*;
 
+@SpringBootApplication
 public class App {
-  
   public static void main(String[] args){
     SpringApplication.run(App.class, args);
   }
