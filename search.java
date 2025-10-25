d71cd458ad0a12334660564716c27498635176e8
Victor Ganson <vicganson@gmail.com>
2025-10-25 14:35:52 -0400

    Update search.java

--- search.java
+++ search.java
@@ -1,20 +1,23 @@
 import java.util.HashMap;
 import java.util.Map;
 public class SearchBar{
-  private HashMap<String, Textbook> listedItems;
+  private HashMap<String, List<Textbook> > listedItems;
   private String search;
   
-  public SearchBar(HashMap<String,Textbook> l, String s){
+  public SearchBar(HashMap<String, List<Textbook> > l, String s){
     this.listedItems = l;
     this.search = s;
   }
 
   public HashMap<> searchResults(String query){
-    Map<String, Textbook> results = new HashMap<>();
+    Map<String, List<Textbook> > results = new HashMap<>();
     for (String s : listedItems){
-      Textbook t = listedItems.get(s);
-      if (t.getCourseCode().contains(query) || t.getTitle().contains(query) || t.getAuthor().contains(query) || t.getISBN().contains(query)){
-        results.put(s, t);
+      List<Textbook> books = new List<>();
+      for (Textbook t : listedItems.get(s)){
+        if (t.getCourseCode().contains(query) || t.getTitle().contains(query) || t.getAuthor().contains(query) || t.getISBN().contains(query)){
+          books.add(t);
+        }
+        results.put(s, books);
       }
     }
     return results;
