9e5275c6a8b1f423dbacf6c39952f41b96cd2abd
HenrySchilling <hankbobmc@gmail.com>
2025-10-26 10:23:01 -0400

    Update SearchController.java

--- src/main/java/com/hacktheheights/controllers/SearchController.java
+++ src/main/java/com/hacktheheights/controllers/SearchController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.*;
 
@@ -59,10 +58,5 @@ public class SearchController {
         return "search";
     }
 
-    @GetMapping("/listings/search")
-    @ResponseBody
-    public List<Textbook> searchListings(@RequestParam String term) {
-        return listings.search(term);
-    }
 
 }
